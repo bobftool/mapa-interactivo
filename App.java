9f5c619107fa4f4dd8194498474370f17b69abfb
bobftool <gonzalezalan321@gmail.com>
2023-06-10 21:10:42 -0600

    Algoritmo para obtener las rutas más cortas

--- App.java
+++ App.java
@@ -2,73 +2,70 @@ public class App {
     public static void main(String[] args){
         Mapa mapa = new Mapa();
 
-        mapa.agregarLugar("Entrada");
-        mapa.agregarLugar("Alimentos");
-        mapa.agregarLugar("Hipopótamos");
-        mapa.agregarLugar("Antílopes");
-        mapa.agregarLugar("Cebras");
-        mapa.agregarLugar("Linces rojos");
-        mapa.agregarLugar("Lobos mexicanos");
-        mapa.agregarLugar("Ajolotes");
-        mapa.agregarLugar("Jaguares");
-        mapa.agregarLugar("Ocelotes");
-        mapa.agregarLugar("Orangutanes");
-        mapa.agregarLugar("Gorilas");
-        mapa.agregarLugar("Pandas");
-        mapa.agregarLugar("Pandas gigantes");
-        mapa.agregarLugar("Tigres");
-        mapa.agregarLugar("Osos");
-        mapa.agregarLugar("Nutrias");
-        mapa.agregarLugar("Conejos");
-        mapa.agregarLugar("Pinguinos");
-        mapa.agregarLugar("Osos polares");
-        mapa.agregarLugar("Paujiles");
-        mapa.agregarLugar("Delfines");
-        mapa.agregarLugar("Cotorras");
-        mapa.agregarLugar("Condor andino");
-        mapa.agregarLugar("Argo real");
-        mapa.agregarLugar("Orcas asesinas");
-        mapa.agregarLugar("Tienda");
-        mapa.agregarLugar("Quiosco");
+        mapa.agregarLugar("Entrada"); //1
+        mapa.agregarLugar("Alimentos"); //2
+        mapa.agregarLugar("Hipopótamos"); //3
+        mapa.agregarLugar("Antílopes"); //4
+        mapa.agregarLugar("Cebras"); //5
+        mapa.agregarLugar("Linces rojos"); //6
+        mapa.agregarLugar("Lobos mexicanos"); //7
+        mapa.agregarLugar("Ajolotes"); //8
+        mapa.agregarLugar("Jaguares"); //9
+        mapa.agregarLugar("Ocelotes"); //10
+        mapa.agregarLugar("Orangutanes"); //11
+        mapa.agregarLugar("Gorilas"); //12
+        mapa.agregarLugar("Pandas gigantes"); //13
+        mapa.agregarLugar("Pandas rojos"); //14
+        mapa.agregarLugar("Tigres"); //15
+        mapa.agregarLugar("Osos"); //16
+        mapa.agregarLugar("Nutrias"); //17
+        mapa.agregarLugar("Conejos"); //18
+        mapa.agregarLugar("Pingüinos"); //19
+        mapa.agregarLugar("Osos polares"); //20
+        mapa.agregarLugar("Paujiles"); //21
+        mapa.agregarLugar("Cotorras"); //22
+        mapa.agregarLugar("Cóndores andinos"); //23
+        mapa.agregarLugar("Argo real"); //24
+        mapa.agregarLugar("Orcas asesinas"); //25
+        mapa.agregarLugar("Tiburones"); //26
+        mapa.agregarLugar("Delfines"); //27
+        mapa.agregarLugar("Quiosco"); //28
 
-        mapa.crearCamino("Entrada", "Lobos mexicanos", 180);
-        mapa.crearCamino("Entrada", "Ajolotes", 200);
-        mapa.crearCamino("Lobos mexicanos", "Linces rojos", 30);
-        mapa.crearCamino("Linces rojos", "Cebras", 40);
-        mapa.crearCamino("Linces rojos", "25", 55);
-        mapa.crearCamino("25", "Cebras", 45);
-        mapa.crearCamino("Cebras", "Hipopótamos", 130);
-        mapa.crearCamino("Hipopótamos", "Antílopes", 60);
-        mapa.crearCamino("Antílopes", "27", 35);
-        mapa.crearCamino("27", "Pinguinos", 80);
-        mapa.crearCamino("27", "Conejos", 50);
-        mapa.crearCamino("27", "Alimentos", 30);
-        mapa.crearCamino("Pinguinos", "Osos polares", 30);
-        mapa.crearCamino("Osos polares", "28", 50);
-        mapa.crearCamino("28", "Nutrias", 50);
-        mapa.crearCamino("28", "Tigres", 45);
-        mapa.crearCamino("28", "Condor andino", 60);
-        mapa.crearCamino("Condor andino", "Paujiles", 50);
-        mapa.crearCamino("Paujiles", "Cotorras", 30);
-        mapa.crearCamino("27", "Pinguinos", 80);
-        mapa.crearCamino("Cotorras", "Argo real", 40);
-        mapa.crearCamino("Argo real", "Condor andino", 30);
-        mapa.crearCamino("Nutrias", "Osos", 30);
-        mapa.crearCamino("Osos", "Tigres", 30);
-        mapa.crearCamino("Tigres", "Pandas", 40);
-        mapa.crearCamino("Pandas", "Conejos", 45);
-        mapa.crearCamino("Pandas", "Pandas gigantes", 40);
-        mapa.crearCamino("Pandas gigantes", "Ajolotes", 35);
-        mapa.crearCamino("Ajolotes", "Alimentos", 20);
-        mapa.crearCamino("Ajolotes", "Jaguares", 30);
-        mapa.crearCamino("Jaguares", "Orangutanes", 10);
-        mapa.crearCamino("Ocelotes", "Jaguares", 10);
-        mapa.crearCamino("Ocelotes", "26", 60);
-        mapa.crearCamino("Gorilas", "26", 50);
+        mapa.crearCamino(1, 7, 160);
+        mapa.crearCamino(1, 8, 220);
+        mapa.crearCamino(2, 8, 40);
+        mapa.crearCamino(2, 27, 80);
+        mapa.crearCamino(3, 4, 80);
+        mapa.crearCamino(3, 5, 140);
+        mapa.crearCamino(4, 27, 30);
+        mapa.crearCamino(5, 6, 60);
+        mapa.crearCamino(5, 25, 60);
+        mapa.crearCamino(6, 25, 60);
+        mapa.crearCamino(6, 7, 50);
+        mapa.crearCamino(8, 9, 50);
+        mapa.crearCamino(8, 13, 60);
+        mapa.crearCamino(9, 10, 30);
+        mapa.crearCamino(9, 11, 20);
+        mapa.crearCamino(10, 26, 110);
+        mapa.crearCamino(12, 13, 90);
+        mapa.crearCamino(12, 26, 150);
+        mapa.crearCamino(13, 14, 50);
+        mapa.crearCamino(14, 15, 60);
+        mapa.crearCamino(14, 18, 50);
+        mapa.crearCamino(15, 16, 30);
+        mapa.crearCamino(16, 17, 40);
+        mapa.crearCamino(16, 28, 50);
+        mapa.crearCamino(17, 18, 20);
+        mapa.crearCamino(18, 27, 80);
+        mapa.crearCamino(19, 20, 30);
+        mapa.crearCamino(19, 27, 70);
+        mapa.crearCamino(20, 28, 50);
+        mapa.crearCamino(21, 22, 20);
+        mapa.crearCamino(21, 23, 30);
+        mapa.crearCamino(22, 24, 50);
+        mapa.crearCamino(23, 24, 20);
+        mapa.crearCamino(23, 28, 80);
 
-        mapa.leer();
-        mapa.imprimir();
-        
-
+        mapa.ir("Lobos mexicanos");
     }
 }
--- Mapa.java
+++ Mapa.java
@@ -1,16 +1,16 @@
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Queue;
-
-import javax.sound.midi.Soundbank;
-import javax.sound.sampled.SourceDataLine;
 
 public class Mapa {
     private final int infinito = Integer.MAX_VALUE;
 
     private List<Lugar> lugares = new ArrayList<>();
     private int[][] caminos;
+
+    private int idUbicacionActual = 0;
 
 
 
@@ -43,22 +43,8 @@ public class Mapa {
 
 
     public void crearCamino(String lugarA, String lugarB, int distancia){
-        int idLugarA = -1;
-        int idLugarB = -1;
-
-        for(int i=0; i<lugares.size(); i++){
-            if(idLugarA < 0 || idLugarB < 0){
-                if(lugares.get(i).getNombre().equals(lugarA)){
-                    idLugarA = i;
-                }
-    
-                if(lugares.get(i).getNombre().equals(lugarB)){
-                    idLugarB = i;
-                }
-            }else{
-                break;
-            }
-        }
+        int idLugarA = buscaridLugar(lugarA);
+        int idLugarB = buscaridLugar(lugarB);
 
         if(idLugarA >= 0 && idLugarB >= 0){
             caminos[idLugarA][idLugarB] = distancia;
@@ -66,6 +52,143 @@ public class Mapa {
         }else{
             System.out.println("No se pudo crear el camino entre "+lugarA+" y "+lugarB+".");
         }
+    }
+
+
+
+    public void crearCamino(int idLugarA, int idLugarB, int distancia){
+        idLugarA-=1;
+        idLugarB-=1;
+
+        if(idLugarA >= 0 && idLugarB >= 0 && idLugarA < lugares.size() && idLugarB < lugares.size()){
+            caminos[idLugarA][idLugarB] = distancia;
+            caminos[idLugarB][idLugarA] = distancia;
+        }else{
+            System.out.println("No se pudo crear el camino entre "+idLugarA+" y "+idLugarB+".");
+        }
+    }
+
+
+
+    public Lugar buscarLugar(String nombre){
+        Lugar lugar = null;
+
+        for(int i=0; i<lugares.size(); i++){
+            if(lugares.get(i).getNombre().equals(nombre)){
+                lugar = lugares.get(i);
+                break;
+            }
+        }
+
+        return lugar;
+    }
+
+
+
+    public int buscaridLugar(String nombre){
+        return lugares.indexOf(buscarLugar(nombre));
+    }
+
+
+
+    public void ir(String destino){
+        int idDestino = buscaridLugar(destino);
+        int[] ruta = rutaHacia(idDestino);
+
+        for(int i=0; i<ruta.length; i++){
+            System.out.print("["+lugares.get(ruta[i]).getNombre()+"] -> ");
+        }
+
+        System.out.println();
+        System.out.println("Distancia: "+distanciaHacia(idDestino));
+
+        idUbicacionActual = idDestino;
+    }
+
+
+
+    private int[] rutasDesdeUbicacionActual(){
+        int[] distancias = new int[lugares.size()];
+        int[] rutas = new int[lugares.size()];
+
+        for (int i=0; i<distancias.length; i++) {
+            distancias[i] = infinito;
+            rutas[i] = -1;
+        }
+        distancias[idUbicacionActual] = 0;
+
+        boolean[] visitados = new boolean[lugares.size()];
+        PriorityQueue<Integer> pendientes = new PriorityQueue<>();
+        pendientes.offer(idUbicacionActual);
+
+        while(!pendientes.isEmpty()){
+            int idActual = pendientes.poll();
+
+            if(visitados[idActual]){
+                continue;
+            }
+
+            visitados[idActual] = true;
+
+            for(int i=0; i<lugares.size(); i++){
+                if(caminos[idActual][i] != infinito && !visitados[i]){
+                    int distancia = distancias[idActual] + caminos[idActual][i];
+
+                    if(distancia < distancias[i]) {
+                        distancias[i] = distancia;
+                        rutas[i] = idActual;
+                    }
+
+                    pendientes.offer(i);
+                }
+            }
+        }
+
+        return rutas;
+    }
+
+    private int[] rutaHacia(int idDestino){
+        int[] rutasDesdeUbicacionActual = rutasDesdeUbicacionActual();
+        int[] rutaInversa = new int[lugares.size()];
+
+        int longitudRuta = 0;
+        int idActual = idDestino;
+
+        while(idActual != -1){
+            rutaInversa[longitudRuta++] = idActual;
+            idActual = rutasDesdeUbicacionActual[idActual];
+        }
+
+        int[] ruta = new int[longitudRuta];
+
+        for(int i=longitudRuta-1; i>=0; i--){
+            ruta[(longitudRuta-1)-i] = rutaInversa[i];
+        }
+
+        return ruta;
+    }
+
+
+
+    private int distanciaHacia(int idDestino){
+        int distancia = 0;
+
+        int[] rutasDesdeUbicacionActual = rutasDesdeUbicacionActual();
+        int[] rutaInversa = new int[lugares.size()];
+
+        int longitudRuta = 0;
+        int idActual = idDestino;
+
+        while(idActual != -1){
+            rutaInversa[longitudRuta++] = idActual;
+            idActual = rutasDesdeUbicacionActual[idActual];
+        }
+
+        for(int i=longitudRuta-2; i>=0; i--){
+            distancia += caminos[idUbicacionActual][rutaInversa[i]];
+        }
+
+        return distancia;
     }
 
 
